077d459562207578ba50da00257ee6b992cbf3e6
Dmytryi <muzalevskyid333@gmail.com>
2026-02-18 17:06:08 +0200

    Implement JordanMethod

---
+++ Lab01_JordanMethods/Lab01.Logic/JordanSolver.cs
@@ -0,0 +1,57 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Lab01.Logic
+{
+    public class JordanSolver
+    {
+        private int n = 3;
+        private double[,] matrixA;
+
+        public JordanSolver()
+        {
+          
+            matrixA = new double[n, n];
+        }
+
+
+
+        public double[,] JordanMethod(double[,] matrixA)
+        {
+            int rowsCount = matrixA.GetLength(0);
+            int columnsCount = matrixA.GetLength(1);
+            double[,] nextMatrix = new double[rowsCount, columnsCount];
+
+            int r = rowsCount / 2;
+            int s = columnsCount / 2;
+
+            double oldPivot = matrixA[r, s];
+
+           
+            nextMatrix[r, s] = 1 / oldPivot;
+
+            for (int i = 0; i < rowsCount; i++)
+            {
+                for (int j = 0; j < columnsCount; j++)
+                {
+                    
+                    if (i == r && j != s)
+                    {
+                        nextMatrix[r, j] = -matrixA[r, j] / oldPivot;
+                    }
+                   
+                    else if (i != r && j == s)
+                    {
+                        nextMatrix[i, s] = matrixA[i, s] / oldPivot;
+                    }
+                    
+                    else if (i != r && j != s)
+                    {
+                        nextMatrix[i, j] = (matrixA[i, j] * oldPivot - matrixA[i, s] * matrixA[r, j]) / oldPivot;
+                    }
+                }
+            }
+
+            return nextMatrix;
+        }
+    }
+}
---
+++ Lab01_JordanMethods/Lab01.Tests/UnitTest1.cs
@@ -0,0 +1,34 @@
+﻿using Lab01.Logic;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using Xunit.Abstractions;
+
+namespace Lab01.Tests
+{
+    public class UnitTest1
+    {
+        private readonly ITestOutputHelper _output;
+        public UnitTest1(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+        [Fact]
+        public void TestJordanSteps()
+        {
+
+            double[,] testMatrix = {
+    { 5, -3, 7 },
+    { -1, 4, 3 },
+    { 6, -2, 5 }
+};
+            var solver = new JordanSolver();
+            var result = solver.JordanMethod(testMatrix); // Твой метод из Class Library
+
+            // Выводим все шаги в консоль тестов
+            foreach (var step in result)
+            {
+                _output.WriteLine(step.ToString());
+                // Тут выводишь матрицу step.CurrentMatrixState
+            }
+        }
+    }
+}
